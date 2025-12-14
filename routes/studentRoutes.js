a80f1f6d29fc7746c63ced2022d8bb0ff2fbb0a0
Jillanegonio18 <jillan.egonio18@gmail.com>
2025-12-14 22:27:53 +0800

    WAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -12,4 +12,20 @@ router.get("/", getAllStudents);
 // pag create hin bag-o nga estudyante
 router.post("/", createStudent);
 
+router.delete("/students/:id", async (req, res) => {
+  try {
+    const student = await Student.findByIdAndDelete(req.params.id);
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    await Attendance.deleteMany({ studentId: student._id });
+
+    res.status(200).json({ message: "Student and attendance records deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 export default router;
